220e02c1126e7ad0b08339528c85123c3449a18e
Charlie McClung <charlie@cmr1.com>
2019-06-23 16:19:36 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+comic_books = ComicBook.create([
+  {
+    name: 'Adventures of a DevOps Guru',
+    description: 'Follow the DevOps Guru through the wild world of managing infrastructure, automation, and processes!',
+    author: 'Charlie McClung'
+  },
+  {
+    name: 'Super Programmer Man',
+    description: 'He\'s a programmer, he knows all the languages, he does all the things. He\'s Super Programmer Man.',
+    author: 'Charlie McClung'
+  }
+])
